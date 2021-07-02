cde028b724a32967c7baa46654f72065b368eb88
antraxr32 <83722528+antraxr32@users.noreply.github.com>
2021-07-01 20:57:33 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'db' => [
+    'host' => '192.168.1.83',
+    'user' => 'user2',
+    'pass' => 'datatest96',
+    'name' => 'tutorial_crud',
+    'options' => [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]
+  ]
+];
